a5dc7c068f665b6dcf6dde86e29f3fbcccc70f0f
Rafał Myśliwczyk <mysliwczykrafal@gmail.com>
2025-05-12 11:29:43 +0200

    Added CORS configuration

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.dependencies import create_db_and_tables
 from app.routers import auth, chat, users
@@ -18,6 +19,14 @@ async def my_lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=my_lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+    allow_credentials=True
+)
 
 app.include_router(auth.router)
 app.include_router(users.router)
